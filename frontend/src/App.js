76af65f027e4d0734f4268eac6634f206b50f0b9
Subham Gupta <56687896+SubhamGupta007@users.noreply.github.com>
2021-06-28 21:45:13 +0530

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,123 +1,45 @@
-import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import Home from './components/Home';
-import About from './components/About';
-import Team from './components/Team';
-import Footer from './components/Footer';
-import { Route, Switch, NavLink } from "react-router-dom"
-import Mail from './components/Mail';
-import { useState } from "react"
-import Future from "./components/Future"
-import History from './components/History';
-import { GoogleLogin, GoogleLogout } from "react-google-login"
-import { GrMail } from "react-icons/gr"
-import PrivateRoute from './components/PrivateRoute'
-
-
-const App = () => {
-  const [user, setuser] = useState(0)
-  localStorage.setItem('user', 0)
-
-
-  const responseGoogle = (response) => {
-    setuser(response.profileObj.googleId)
-    localStorage.setItem('user', response.profileObj.googleId)
-  }
-
-  const load = () => {
-    window.location.reload();
-  }
-
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-warning " >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/"><GrMail size={32} /> </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav">
-              <li className="nav-item  ">
-                <NavLink to="/" className="nav-link active  " aria-current="page">Home </NavLink>
-
-              </li>
-              <li className="nav-item">
-                <NavLink to="/team" className="nav-link active  " aria-current="page">Team</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/history" className="nav-link active  " aria-current="page">History</NavLink>
-              </li>
-
-              <li className="nav-item">
-                <NavLink to="/futuremails" className="nav-link active  " aria-current="page">Scheduled Mails</NavLink>
-              </li>
+import { React } from 'react'
+import { Link } from 'react-router-dom'
+import Zoom from 'react-reveal/Zoom';
 
 
 
-            </ul>
 
-            {
-              (user == 0) ?
-                <div className="">
-                  <GoogleLogin
-                    clientId="1099252555614-t7njjdio9amae9b7d52oc8qju53nlb2h.apps.googleusercontent.com"
-                    buttonText="Login"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    isSignedIn={true}
-                    cookiePolicy={'single_host_origin'} />
+const Home = ({ user }) => {
+
+
+
+    return (
+        <>
+            <section className="Home mt-3">
+                <div className="container-fluid">
+                    <div className="row bg-transparent  vh-100 align-content-center align-items-center">
+                        <div className="col-md-6 ">
+                            <div className="content  py-3">
+                                <div className="text-left px-5">
+                                    <Zoom>
+                                        <h4 className="  text-dark justify-content-lg-start  fst-italic">-Welcome to</h4>
+                                        <h2 className=" fst-italic">Email Scheduling Application</h2>
+                                        <h5 className="text-muted">An online application where you can schedule your E-mails </h5>
+                                    </Zoom>
+                                </div>
+
+                                {
+                                    user ? <Link to="/mail" className="btn btn-warning m-5 p-3" style={{ width: "110px" }}>Send Mail</Link> : null
+
+                                }
+
+                            </div>
+                        </div>
+                        <div className="col-md-6 order-first-md">
+                            <img style={{ width: "100%" }} src="/images/bg.jpg" alt="img"></img>
+                        </div>
+                    </div>
                 </div>
-                :
-                <>
-                  <NavLink to="/mail" className="nav-link active text-dark text-bold " aria-current="page">Send Mail</NavLink>
-                  <button onClick={load} className="p-0"> <GoogleLogout
-                    clientId="1099252555614-t7njjdio9amae9b7d52oc8qju53nlb2h.apps.googleusercontent.com"
-                    buttonText="Logout"
-                  /></button>
+            </section>
 
-
-                </>
-            }
-          </div>
-        </div>
-      </nav>
-
-
-      <Switch>
-
-        <Route exact path="/">
-
-          <Home user={user} />
-
-          <About />
-
-          <Team />
-
-        </Route>
-
-        <PrivateRoute exact path="/futuremails" component={Future} />
-
-        <PrivateRoute exact path="/history" component={History} />
-
-        <PrivateRoute exact path="/mail" component={Mail} />
-
-        <Route exact path="/team">
-
-          <Team />
-
-        </Route>
-
-      </Switch>
-      <Footer />
-    </>
-  );
+        </>
+    )
 }
-export const isLogin = () => {
-  if (localStorage.getItem('user') != 0) {
-    return true;
-  }
-  return false;
 
-}
-export default App;
+export default Home
